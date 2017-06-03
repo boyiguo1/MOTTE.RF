eab2563aabd50a8ddf1e355127ad505491a23240
Boyi Guo <boyiguo1992@gmail.com>
2017-06-03 00:27:49 -0500

    Add error prevention in buildForest

--- buildForest.R
+++ buildForest.R
@@ -34,7 +34,38 @@ buildForest <- function(
   nCore=ifelse(is.null(nsplits),1,detectCores()-1)) {
   
   ### Error Prevention Code
+  if(!is.matrix(x.b) || !is.matrix(x.e) || !is.matrix(y.b) || !is.matrix(y.e))
+    stop("Error Message: x.b, x.e, y.b, y.e must be matrices")
   
+  if(!is.vector(treat))
+    stop("Error Message: treat must be a vector")
+  
+  if(length(unique(
+    nrow(x.b),nrow(x.e),length(treat),nrow(y.b), nrow(y.b)
+  ))!=1)
+    stop("Error Message: incosistent observation numbers in x.b, x.e, treat, y.b, y.e")
+  
+  if(!is.numeric(left.out) || !is.between(left.out,0,0.5))
+    stop("Error Message: left.out must be a numeric value between 0 and 0.5")
+
+  if(!is.null(nsplits) & !is.integer(nsplits))
+    stop("Error Message: nsplits must be NULL or numeric")
+  
+  nCore <- round(nCore)
+  if(!is.numeric(nCore) || nCore <= 0)
+    stop("Error Message: nCore must be a positive integer")
+
+  ntree <- round(ntree)  
+  if(!is.numeric(ntree) || ntree <= 0)
+    stop("Error Message: ntree must be a positive integer")
+    
+  if(is.null(nsplits))
+    cat("Exhaustively searching for split value\n")
+  
+  treat <- factor(treat)
+  if(length(levels(treat)) != 2)
+    stop("Error Message: Incorrect number of treatment groups. Must be 2 groups")
+
   if(nCore==1)
   {
     forest <- lapply(1:ntree,FUN = function(x){
