bd7c713684b685fb4f8b39cf10b07359d03e5c5a
Boyi Guo <boyiguo1@uab.edu>
2020-01-22 09:51:07 -0600

    Format Change

--- R/sim_MOTTE_data.R
+++ R/sim_MOTTE_data.R
@@ -5,36 +5,34 @@
 #' @param p An integer value, the dimensionality of covaraites (X)
 #' @param q An integer value, the dimensionality of responses (Y)
 #' @param pi An fraction value, the ratio between treatment groups
-#' @param seed An integer value, set the seed number
+#' #@param seed An integer value, set the seed number
 #'
-#' @return A nested list that contain training data and testing data
+#' @return A nested list that contain training data and testing data. TODO: add more introdcution to whats in the list
 #' @export
 #' @importFrom MASS mvrnorm
 #'
 #' @examples
+#' set.seed(1)
 #' sim_MOTTE_data( n.train = 500, n.test = 200,
 #' p = 10, q = 3, pi = 0.5)
 
 
 sim_MOTTE_data <- function(
-  n.train, # <-  500
-  n.test, # <- 200
-  p, #  <- 10
-  q, #  <- 3
+  n.train = 500, # <-  500
+  n.test = 200, # <- 200
+  p = 10, #  <- 10
+  q = 3, #  <- 3
   # pi is the ratio between treatment groups
-  pi, # <- 0.5,
-  seed = 1
+  ratio = 0.5#, # <- 0.5,
+  #seed = 1
 ){
-  set.seed(seed)
-  # Simulate the treatment
-  Treat.train <- sample(
-    c(
-      rep(1, floor(n.train*pi)),           # Treatment Group 1
-      rep(0, ceiling(n.train*(1-pi)))      # Treatment Group 2
-    )
-  )
+  #set.seed(seed)
+  # Simulate the binary treatment assignment for training data
+  Treat.train <- rbinom(n.train, 1, ratio)
 
-  # Beta matrix
+  # Set up for the design matrix
+  # TODO: extract this as an argument
+  # figure out how this has been in the paper
   Z <- matrix(
     cbind(
       c(rep(1,5),rep(0,5)),
@@ -46,16 +44,17 @@ sim_MOTTE_data <- function(
   x.sig <- 0.8^(abs(outer(1:p,1:p,"-")))
 
   # Simulate x.b
-  #X.train.base <- mvrnorm(n.train,rep(0,p),x.sig)
   X.train.base <- MASS::mvrnorm(n.train,rep(0,p),diag(p))
 
-
+  # TODO: why the treatment effect have the same i, and j
+  # TODO: Extract treat.effect as a parameter
   X.train.end <- X.train.base
   X.train.end[,5] <- X.train.end[,5] + treat.effect(X.train.base,Treat.train,1,3)
   X.train.end[,6] <- X.train.end[,6] + treat.effect(X.train.base,Treat.train,1,3)
-  X.train.end[,7]<- X.train.end[,7] + treat.effect(X.train.base,Treat.train,1,3)
+  X.train.end[,7] <- X.train.end[,7] + treat.effect(X.train.base,Treat.train,1,3)
   X.train.end <- X.train.end + mvrnorm(n.train, rep(0,p), 0.01*diag(p))
 
+  #TODO: fix the sd here. Too small
   Y.train.base <- (X.train.base)%*%Z + mvrnorm(n.train, rep(0,q), 0.01*diag(q))
   Y.train.end <- (X.train.end)%*%Z + mvrnorm(n.train, rep(0,q), 0.01*diag(q))
 
@@ -65,7 +64,6 @@ sim_MOTTE_data <- function(
   #
   ####################################
 
-  #X.test.base  <- mvrnorm(n.test,rep(0,p),x.sig)
   X.test.base  <- MASS::mvrnorm(n.test,rep(0,p),diag(p))
   # With/Without treatment X.end
   X.test.case.end <-  X.test.base
@@ -82,7 +80,6 @@ sim_MOTTE_data <- function(
   Y.test.case.end <- (X.test.case.end)%*%Z
   Y.test.control.end <- (X.test.control.end)%*%Z
 
-  # TODO: add return list
   return(
     list(
       train = list(x.b = X.train.base,
