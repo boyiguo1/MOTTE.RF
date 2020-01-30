0f05e85d2b2a1c89fe63b55d08f657fc742bd03a
Boyi Guo <boyiguo1@uab.edu>
2020-01-29 18:31:22 -0600

    Improved the simulation functions

--- R/sim_MOTTE_data.R
+++ R/sim_MOTTE_data.R
@@ -13,21 +13,10 @@
 #'
 #' @examples
 #' set.seed(1)
-#' #TODO: extract making B to a function
-#' B <- matrix(
-#'     c(rep(1,3), rep(0, 7),
-#'     rep(0,3), rep(1,3), rep(0, 4),
-#'     rep(0,6), rep(1,3), 0),
-#'     nrow = 10, ncol = 3) %>%
-#'     cbind(matrix(0,nrow = 10, ncol = 7))
+#' B <- create.B(10)
+#' Z <- create.Z(10, 3)
 #'
-#' #TODO: extract making Z to a function
-#' Z <- matrix(
-#'    c(rep(1,3), rep(0, 7),
-#'     rep(0,3), rep(1,3), rep(0, 4),
-#'     rep(0,6), rep(1,3), 0),
-#'     nrow = 10, ncol = 3
-#' )
+#' set.seed(1)
 #' sim_MOTTE_data( n.train = 500, n.test = 200,
 #' p = 10, q = 3, ratio = 0.5,
 #' B = B, Z = Z)
@@ -39,33 +28,51 @@ sim_MOTTE_data <- function(
   n.test = 200, # <- 200
   p = 10, #  <- 10
   q = 3, #  <- 3
-  # pi is the ratio between treatment groups
   ratio = 0.5, # <- 0.5,
+  # TODO: set up error prevention for cov.mat, i.e. measuring the dimension
   cov.mat = diag(p),
   # TODO: incorporate the linear and polynormial for both treat.f and link.f in the code
-  treat.f = c("Linear", "Polynomial"),
+  trt.f = c("Linear", "Polynomial"),
   link.f = c("Linear", "Polynomial"),
   B ,
   Z
   #seed = 1
 ){
-  #set.seed(seed)
+
+  # TODO: extract this as argument for the parameter
+  c.x <- 0.01
+  c.y <- 0.01
+
+  trt.f <- trt.f[[1]]
+  link.f <- link.f[[1]]
+
+  # TODO: Improve the warning language
+  .link.f <- switch(link.f,
+                    "Linear" = function(x){x%*%Z},
+                    "Polynomial" = function(x){(x^2)%*%Z},
+                    stop("Link function doesn't exist, choose from 'Linear' or 'Polynomial'"))
+  .trt.f <- switch(trt.f,
+                   "Linear" = function(x, trt){sweep(x, 1, trt, "*")%*%B},
+                   "Polynomial" = function(x, trt){(sweep(x, 1, trt, "*")^2)%*%B},
+                   stop("Trt.f doesn't exist, choose from 'Linear' or 'Polynomial'"))
+
+
   # Simulate the binary treatment assignment for training data
+  # Clarification, when trt1, X.e = X.b + X.b%*%B and trt2, X.2 =X.b - X.b%*%B
   Trt.lvls <- c("Trt 1", "Trt 2")
   Trt.train <- factor(Trt.lvls[rbinom(n.train, 1, ratio)+1])
 
   # Simulate the autoregressive covariance matrix of X
-  # TODO: replace all x.sig with cov.mat
-  x.sig <- 0.8^(abs(outer(1:p,1:p,"-")))
+  # cov.mat <- 0.8^(abs(outer(1:p,1:p,"-")))
 
   # Simulate x.b
-  X.train.base <- MASS::mvrnorm(n.train,rep(0,p),diag(p))
+  X.train.base <- MASS::mvrnorm(n.train,rep(0,p), cov.mat)
 
-  # TODO: add treatment effect in it by making treat.train in it.
-  X.train.end <- X.train.base + X.train.base %*% B + mvrnorm(n.train, rep(0,p), 0.01*diag(p))
 
-  Y.train.base <- (X.train.base)%*%Z + mvrnorm(n.train, rep(0,q), 0.01*diag(q))
-  Y.train.end <- (X.train.end)%*%Z + mvrnorm(n.train, rep(0,q), 0.01*diag(q))
+  X.train.end <- X.train.base + .trt.f(X.train.base, ifelse(Trt.train=="Trt 1", 1, -1)) + mvrnorm(n.train, rep(0,p), c.x*diag(p))
+
+  Y.train.base <- .link.f(X.train.base) + mvrnorm(n.train, rep(0,q), c.y*diag(q))
+  Y.train.end <- .link.f(X.train.end) + mvrnorm(n.train, rep(0,q), c.y*diag(q))
 
 
   ####################################
@@ -75,26 +82,69 @@ sim_MOTTE_data <- function(
 
   X.test.base  <- MASS::mvrnorm(n.test,rep(0,p),diag(p))
   # With/Without treatment X.end
-  # TODO: change the matrix calc to add treatment effect on top
-  X.test.case.end <-  X.test.base + X.test.base %*% B
-  X.test.control.end <-  X.test.base + X.test.base %*% B
+  X.test.trt1.end <-  X.test.base + .trt.f(X.test.base, 1)
+  X.test.trt2.end <-  X.test.base + .trt.f(X.test.base, -1)
+
 
   # With/Without treatment Y.base
-  Y.test.case.end <- (X.test.case.end)%*%Z
-  Y.test.control.end <- (X.test.control.end)%*%Z
+  Y.test.trt1.end <- .link.f(X.test.trt1.end)
+  Y.test.trt2.end <- .link.f(X.test.trt2.end)
 
   return(
     list(
       train = list(x.b = X.train.base,
                   x.e = X.train.end,
-                  treat = Treat.train,
+                  trt = Trt.train,
                   y.b = Y.train.base,
                   y.e = Y.train.end),
       test = list(
         x.b = X.test.base,
-        y.e.case = Y.test.case.end,
-        y.e.control = Y.test.control.end
+        y.e.case = Y.test.trt1.end,
+        y.e.control = Y.test.trt2.end
       )
     )
+  )
+}
+
+#' Title
+#'
+#' @param p The dimension of the design matrix
+#'
+#' @return A matrix
+#' @export
+#'
+#' @examples
+#' create.B(10)
+create.B <- function(p){
+  if(p < 9)
+    stop("Minimum value for p is 9")
+  cbind(
+    matrix(
+      c(rep(1,3), rep(0, p-3),
+        rep(0,3), rep(1,3), rep(0, p-6),
+        rep(0,6), rep(1,3), rep(0, p-9)),
+      nrow = p, ncol = 3),
+    matrix(0,nrow = p, ncol = p-3)
+  )
+}
+
+#' Title
+#'
+#' @param p The number of rows
+#' @param q The number of columns
+#'
+#' @return A design matrix
+#' @export
+#'
+#' @examples
+#' create.z(10,3)
+create.Z <- function(p, q){
+  matrix(
+    c(rep(1,3), rep(0, p-3),
+      rep(0,3), rep(1,3), rep(0, p-6),
+      rep(0,6), rep(1,3), rep(0, p-9),
+      rep(0, p*(q-3))
+      ),
+    nrow = p, ncol = q
   )
 }
