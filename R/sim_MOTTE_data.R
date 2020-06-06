92829de19ae3deff13af774af21c23c334c43073
Boyi Guo <boyiguo1@uab.edu>
2020-06-06 13:19:08 -0500

    adjust signal

--- R/sim_MOTTE_data.R
+++ R/sim_MOTTE_data.R
@@ -70,8 +70,8 @@ sim_MOTTE_data <- function(
 
   # TODO: Improve the warning language
   .link.f <- switch(link.f,
-                    "Linear" = function(x){x%*%Z/5},
-                    "Polynomial" = function(x){(x^2)%*%Z/5},
+                    "Linear" = function(x){x%*%Z/3},
+                    "Polynomial" = function(x){(x^2)%*%Z/3},
                     stop("Link function doesn't exist, choose from 'Linear' or 'Polynomial'"))
   .trt.f <- switch(trt.f,
                    "Linear" = function(x, trt){sweep(cbind(1,x), 1, trt, "*")%*%B},
