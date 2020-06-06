8c7127d7119e5215656805ca8552fb07ad0cb5ff
Boyi Guo <boyiguo1@uab.edu>
2020-06-06 15:28:40 -0500

    try to make signal in X larger y smaller

--- R/sim_MOTTE_data.R
+++ R/sim_MOTTE_data.R
@@ -61,7 +61,7 @@ sim_MOTTE_data <- function(
 
   # TODO: extract this as argument for the parameter
   c.x <- sum((1:3)^2)/6
-  c.y <- sum((1:3)^2)/6
+  c.y <- sum((1:3)^2)/2
 
   # c.x <- 0.5
   # c.y <- 0.5
@@ -70,8 +70,8 @@ sim_MOTTE_data <- function(
 
   # TODO: Improve the warning language
   .link.f <- switch(link.f,
-                    "Linear" = function(x){x%*%Z},
-                    "Polynomial" = function(x){(x^2)%*%Z},
+                    "Linear" = function(x){x%*%Z/2},
+                    "Polynomial" = function(x){(x^2)%*%Z/2},
                     stop("Link function doesn't exist, choose from 'Linear' or 'Polynomial'"))
   .trt.f <- switch(trt.f,
                    "Linear" = function(x, trt){sweep(cbind(1,x), 1, trt, "*")%*%B},
@@ -150,19 +150,19 @@ create.B <- function(p, intercept=T){
   rbind(ifelse(intercept, 1, 0),
         cbind(
           matrix(
-            c(1:3/sqrt(20), rep(0, p-3),
-              rep(0,3), 1:3/sqrt(20), rep(0, p-6),
-              rep(0,6), 1:3/sqrt(20), rep(0, p-9)),
+            c(1:3/sqrt(10), rep(0, p-3),
+              rep(0,3), 1:3/sqrt(10), rep(0, p-6),
+              rep(0,6), 1:3/sqrt(10), rep(0, p-9)),
             nrow = p, ncol = 3),
           -1*matrix(
-            c(1:3/sqrt(20), rep(0, p-3),
-              rep(0,3), 1:3/sqrt(20), rep(0, p-6),
-              rep(0,6), 1:3/sqrt(20), rep(0, p-9)),
+            c(1:3/sqrt(10), rep(0, p-3),
+              rep(0,3), 1:3/sqrt(10), rep(0, p-6),
+              rep(0,6), 1:3/sqrt(10), rep(0, p-9)),
             nrow = p, ncol = 3),
           matrix(
-            c(c(1,-1,1)*(1:3)/sqrt(20), rep(0, p-3),
-              rep(0,3), c(-1,1,-1)*(1:3)/sqrt(20), rep(0, p-6),
-              rep(0,6), c(1,1,-1)*(1:3)/sqrt(20), rep(0, p-9)),
+            c(c(1,-1,1)*(1:3)/sqrt(10), rep(0, p-3),
+              rep(0,3), c(-1,1,-1)*(1:3)/sqrt(10), rep(0, p-6),
+              rep(0,6), c(1,1,-1)*(1:3)/sqrt(10), rep(0, p-9)),
             nrow = p, ncol = 3),
           matrix(0,nrow = p, ncol = p-9)
         )
